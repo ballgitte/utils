a9a7ad33c80392844f071b05c171232c58d281e5
ballgitte <mail@ballgitte.dev>
2026-01-27 11:55:26 -0600

    Changed build.gradle to build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+group = "dev.ballgitte"
+version = "1.2.0"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = "dev.ballgitte"
+            artifactId = "utils"
+            version = "1.2.1"
+        }
+    }
+    repositories {
+        maven {
+            name = "localRepo"
+            url = uri(System.getProperty("user.home") + "/.m2/repository")
+        }
+    }
+}
