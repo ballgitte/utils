416c18f29845c931d44765bd4bd29f0f8e551133
ballgitte <mail@ballgitte.dev>
2026-01-28 13:32:31 -0600

    Fixed version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "dev.ballgitte"
-version = "1.3.2"
+version = "1.3.3"
 
 java {
     toolchain {
@@ -18,7 +18,7 @@ publishing {
             from(components["java"])
             groupId = "dev.ballgitte"
             artifactId = "utils"
-            version = "1.3.2"
+            version = "1.3.3"
         }
     }
     repositories {
